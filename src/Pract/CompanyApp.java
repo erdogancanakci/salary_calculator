abf2e3c516b034dba4e6c1787e1715380b18d9e7
erdogan <erdogancanakci@gmail.com>
2022-10-25 23:43:35 +0300

    refactor

--- src/Pract/CompanyApp.java
+++ src/Pract/CompanyApp.java
@@ -3,6 +3,7 @@ package Pract;
 public class CompanyApp {
     public static void main(String[] args) {
         Employee e = new Employee();
+
         e.setName("kemal");
         e.setSalary(2000);
         e.setWorkHours(45);
